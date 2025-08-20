933ed1054c43e3fff95b4cdc1157a36ccf928b51
Violetta Vinakova <vinakova.vita@gmail.com>
2025-08-20 23:09:35 +0300

    feat: add copy-webpack-plugin to handle asset copying

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -16,6 +17,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "index.html",
       filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "assets/images/heroes", to: "assets/images/heroes" }],
     }),
   ],
 
